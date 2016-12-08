1bb085981f0293e8f913ee0aefe6aa794fb77456
whitegl1 <gwhite@corp.gwitsolutions.com>
2016-12-08 14:36:15 -0500

    Changes
    Added Icons, more functionality, scripts ect.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
 
 
-$("#siteloader")
-  .html('<object data="https://na.myconnectwise.net/gwit"/>');
+
+
+$('#moduleload').click(function(){
+  $("#siteloader").html('<object data="http://10.0.0.70:8000/inventory"/>');
+});
+
+$('#supportload').click(function(){
+  $("#siteloader").html('<object data="https://na.myconnectwise.net/gwit"/>');
+});
